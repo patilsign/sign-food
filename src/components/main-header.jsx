13c8e86ff9859f568fadbd8b817ba7fb275b12de
patilsign <patilsign108@gmail.com>
2025-03-16 04:00:05 +0530

    added image with predefined nextjs Image tag

--- src/components/main-header.jsx
+++ src/components/main-header.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import classes from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
 import Link from "next/link";
+import Image from "next/image";
 
 const MainHeader = () => {
   return (
     <>
       <header className={classes.header}>
         <Link className={classes.logo} href="/">
-          <img src={logoImg.src} alt="fast food logo" />
+          <Image src={logoImg} alt="fast food logo" priority />
           <h2>Fast Food App</h2>
         </Link>
 
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Fast Food Meals</Link>
+              <a>
+                <Link href="/meals">Fast Food Meals</Link>
+              </a>
             </li>
             <li>
-              <Link href="/community">Community For Meals</Link>
+              <a>
+                <Link href="/community">Community For Meals</Link>
+              </a>
             </li>
           </ul>
         </nav>
